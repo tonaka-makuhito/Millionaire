d1acc5310746957d9c442e65511db92ab976f3e0
tonaka-makuhito <tonaka@makuhito.ru>
2024-03-02 12:10:40 +0000

    greeting

--- millionaire.py
+++ millionaire.py
@@ -1,2 +1,10 @@
 #start
 #game "millionaire"
+
+def greeting():
+    print('Привет, будущий миллионер!')
+    with open('rules.txt', encoding='utf-8') as f:
+        greet = f.read()
+        print(greet)
+        
+greeting()
