b85807e5aacf69403526145a2f61375f592c3f3e
tonaka-makuhito <tonaka@makuhito.ru>
2024-03-17 13:58:33 +0000

    create venv

--- millionaire.py
+++ millionaire.py
@@ -1,10 +1,21 @@
 #start
 #game "millionaire"
+import keyboard
+
+game_on = True
 
 def greeting():
     print('Привет, будущий миллионер!')
     with open('rules.txt', encoding='utf-8') as f:
         greet = f.read()
         print(greet)
-        
-greeting()
+
+def on_key_press(event):
+    if event.name == 'esc':
+        game_on= False
+
+keyboard.on_press(on_key_press)
+
+while game_on:
+    greeting()
+    keyboard.wait()
